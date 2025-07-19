f9fb593e4bf93c2718e52f78ef8d8602def54e7f
Israel Paez <ipaez@ford.com>
2025-07-18 20:50:44 -0600

    First Commit

--- src/main/java/com/artesanalbeer/artesanalbeerstore/ArtesanalbeerstoreApplication.java
+++ src/main/java/com/artesanalbeer/artesanalbeerstore/ArtesanalbeerstoreApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArtesanalbeerstoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtesanalbeerstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArtesanalbeerstoreApplication.class, args);
+    }
 
 }
