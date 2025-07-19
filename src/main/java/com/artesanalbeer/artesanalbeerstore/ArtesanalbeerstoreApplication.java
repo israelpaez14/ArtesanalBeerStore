13cad1d07d749bab293d2c7ca3da97bce0d27d1d
Israel Paez <ipaez@ford.com>
2025-07-18 20:50:25 -0600

    first commit

---
+++ src/main/java/com/artesanalbeer/artesanalbeerstore/ArtesanalbeerstoreApplication.java
@@ -0,0 +1,13 @@
+package com.artesanalbeer.artesanalbeerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesanalbeerstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtesanalbeerstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artesanalbeer/artesanalbeerstore/ArtesanalbeerstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artesanalbeer.artesanalbeerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtesanalbeerstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
