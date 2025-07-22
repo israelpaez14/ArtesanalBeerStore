e5f12dedf807fa18a5394b1b81589c07de04e92e
Israel Paez <ipaez@ford.com>
2025-07-21 18:43:56 -0600

    feat: Added validation annotations to BeerRequest

--- src/main/java/com/artesanalbeer/artesanalbeerstore/dto/beer/BeerRequest.java
+++ src/main/java/com/artesanalbeer/artesanalbeerstore/dto/beer/BeerRequest.java
@@ -1,5 +1,7 @@
 package com.artesanalbeer.artesanalbeerstore.dto.beer;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -11,9 +13,23 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BeerRequest {
+    @NotNull(message = "The beer name is required")
+    @NotBlank(message = "The beer name is required")
     private String name;
+
+    @NotNull(message = "A description is required")
+    @NotBlank(message = "A description is required")
     private String description;
+
+    @NotNull(message = "A beer type is required")
+    @NotBlank(message = "A beer type is required")
     private UUID beerTypeId;
+
+    @NotNull(message = "A released date is required")
+    @NotBlank(message = "A released date is required")
     private LocalDate releasedAt;
+
+    @NotNull(message = "Alcohol percentage is required")
+    @NotBlank(message = "Alcohol percentage is required")
     private Integer alcoholPercentage;
 }
